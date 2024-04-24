635bdfd56e9a3eec842e438d4bb73b58c768f4d3
S304 <monk_su@yahoo.com.tw>
2024-04-24 18:08:46 +0800

    feat(product): 利用事件繫結設定產品顯示狀態

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, booleanAttribute, numberAttribute } from '@angular/core';
+import { Component, EventEmitter, Input, Output, booleanAttribute, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -15,7 +15,9 @@ export class ProductCardComponent {
   @Input({ transform: booleanAttribute }) isShow!: boolean;
   @Input() imgUrl!: string;
 
+  @Output() isShowChange = new EventEmitter<boolean>();
+
   onSetDisplay(isShow: boolean): void {
-    this.isShow = isShow;
+    this.isShowChange.emit(isShow);
   }
 }
