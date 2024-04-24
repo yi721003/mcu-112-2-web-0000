a0a061df6871e012d0a13f6f1747df06f583ac82
S304 <monk_su@yahoo.com.tw>
2024-04-24 17:22:49 +0800

    feat(product): 利用屬性繫結實作圖片位址繫結

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -12,6 +12,7 @@ export class ProductCardComponent {
   authors = '作者甲、作者乙、作者丙';
   company = '博碩文件';
   isShow = true;
+  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
 
   onSetDisplay(isShow: boolean): void {
     this.isShow = isShow;
